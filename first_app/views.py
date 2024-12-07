484b6c644364e9a03c7573f0b67038d2ef78f9ab
Anton Sadikov <antonsadikov1@gmail.com>
2024-12-07 03:12:56 +0300

    all

--- first_app/views.py
+++ first_app/views.py
@@ -1,3 +1,9 @@
+from lib2to3.fixes.fix_input import context
+
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context1 = {
+        "date": "07.12.2024"
+    }
+    return render(request, "index.html", context1)
